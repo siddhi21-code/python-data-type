ca58afe9682d95fe2873d91e510dc78007e6cdca
siddhi21-code <siddhishirke508@gmail.com>
2025-09-05 10:50:36 +0530

    data type

---
+++ demo3.py
@@ -0,0 +1,32 @@
+#integer
+x=5
+print(x)
+
+#floats
+y = 3.14
+print(y)
+
+#string
+z = "hello sidd"
+print(z)
+
+#boolean
+is_active  = True
+print(is_active)
+
+#list
+cars =["thar","bmw","audi","ferrari","lamborghini","porsche","rollace royce","range rover","triber"]
+print (cars)
+
+#disctionary
+#product = {"name":"laptop","brand":"dell"}
+# print ("product:",product)
+
+# tuple
+ #colors =("red","green","blue")
+#print(colors)
+#print(colors[0])
+
+# set
+unique_numbers ={ 1, 2, 3, 4, 5}
+print(unique_numbers)
